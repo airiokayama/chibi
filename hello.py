2f1e69468738c167862334ec8a12a5e8adf201f2
OKAYAMA Airi <21716030@jwu.ac.jp>
2019-11-25 15:46:28 +0900

    hello2

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print("hello")
+import pegpy
+peg = pegpy.grammar('''
+Expression = Product (^{ '+' Product #Add })*
+Product = Value (^{ '*' Value #Mul })*
+Value = { [0-9]+ #Int }
+''')
+parser = pegpy.generate(peg)
+t = parser('1+2*3')
+print(repr(t))
+t = parser('@2')
+print(repr(t))
